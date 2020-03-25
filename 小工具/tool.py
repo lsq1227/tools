42b367d7d7674e7e55f46b3c8c683987b81f8c4e
lsq1227 <1355207916@qq.com>
2020-03-25 14:36:57 +0800

    淘宝直通车图

--- 小工具/tool.py
+++ 小工具/tool.py
@@ -17,10 +17,10 @@ def get_ip(url,Agreement='https',choice=False):
             with open('./ip_pool.txt', 'w', encoding='utf-8') as f:
                 f.write(res)
             res = json.loads(res)
-            return {Agreement:res['data'][0]['ip']+':'+str(+res['data'][0]['port'])}
+            return {Agreement: res['data'][0]['ip']+':'+str(+res['data'][0]['port'])}
         else:
             temp = json.loads(c)
-            return {Agreement:temp['data'][0]['ip']+":"+str(temp['data'][0]['port'])}
+            return {Agreement: temp['data'][0]['ip']+":"+str(temp['data'][0]['port'])}
 
 
 def en_de(data,choice,code='utf-8'):
@@ -74,6 +74,57 @@ def get_page(page,dic):  # 分页器
 
 
 
+def che_get_img(url,img_name,path):
+    """
+    Download the component, fill in the response parameters,
+    and write the IMG file in the relative path
+    :param url:  Picture url to download
+    :param img_name: Picture name to store
+    :param path: File storage path
+    :return: None
+    -----------    Be  tb_img_che()  called ------------------
+    """
+    try:
+        r = requests.get(url,stream=True)
+    except requests.exceptions.MissingSchema:
+        r = requests.get('http:'+url,stream=True)
+    print(f'now ruequests{img_name}')
+    print(r.status_code)
+    name = img_name.replace('/','-').replace('【','').replace('】','').replace('<','').replace('>','')
+    if r.status_code == 200:
+        try:
+            open(f'{path}\{name}.png', 'wb').write(r.content)
+        except:
+            open(f'{path}\{"untitled"+str(random.randint(10000))}.png', 'wb').write(r.content)
+
+
+
+
+def tb_img_che(key,head):
+    """
+    Input the keywords to be searched in TaoBao and TianMao to download the through train pictures。
+    :param key: search key -- string
+    :return: None
+    """
+    path = os.getcwd() + '\\' + f'{key}_train'
+    url = f'https://s.taobao.com/search?q={key}&imgfile=&commend=all&ssid=s5-e&search_type=item&sourceId=tb.index&spm=a21bo.2017.201856-taobao-item.1&ie=utf8&initiative_id=tbindexz_20170306&sort=sale-desc'
+    res = requests.get(url,headers=head).text
+    try:
+        img = re.findall('"pic_url":"(.*?)",',res)
+        shop_name = re.findall('"nick":"(.*?)",',res)
+        id = re.findall('"nid":"(.*?)",',res)
+        err = re.findall('"nid":"(.*?)",',res)[0]
+    except:
+        print('Please change the cookie')
+        head['cookie'] = input('string --> Enter cookies here: ')
+        res = requests.get(url, headers=head).text
+        img = re.findall('"pic_url":"(.*?)",', res)
+        shop_name = re.findall('"nick":"(.*?)",', res)
+        id = re.findall('"nid":"(.*?)",', res)
+    mkdir(path)
+    for i in range(0,len(img)):
+        che_get_img('http:'+img[i],shop_name[i]+'-'+str(id[i]),path)
+        print(shop_name[i])
 
 
 
